e310f9cd15c132fa166e8c955c1c5e47e72dc4c6
Nesterok Andrey <and-nesterok@mail.ru>
2025-07-18 20:14:41 +0400

    feat: added two different ecosystem.config.js files for frontend and backend

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,36 @@
+require("dotenv").config({ path: "../.env.deploy" });
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_REF,
+  DEPLOY_REPO,
+  DEPLOY_PATH_BACKEND,
+  NPM_PATH,
+} = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: "api-service",
+      script: "./dist/app.js",
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH_BACKEND,
+      
+      "pre-deploy-local": `scp ../.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH_BACKEND}/.env`,
+      
+      "post-deploy": `export ${NPM_PATH} && npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production`,
+    },
+  },
+};
